08e36d19a295939e383dd8291d02b39b614620c9
safinsingh <safin.singh@gmail.com>
2020-10-22 10:14:24 -0700

    feat: working bar

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # bamboo
 
 A highly configurable bar for the X Window System
+
+## Dependencies
+
+```sh
+apt install pkg-config libglib2.0-dev libcairo2-dev
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,41 @@
 use serde::Deserialize;
-
+use std::collections::HashMap;
 #[derive(Deserialize, Debug)]
 pub struct Bar {
-	width: usize,
-	height: usize,
-	center: bool,
-	bottom: Option<bool>,
-	#[serde(rename = "border-width")]
-	border_width: Option<i32>,
+	pub width: u16,
+	pub height: u16,
+	pub center: bool,
+	#[serde(default = "default_false")]
+	pub bottom: bool,
+	#[serde(rename = "border-width", default = "default_zero")]
+	pub border_width: u16,
 	#[serde(rename = "offset-x")]
-	offset_x: usize,
+	pub offset_x: i16,
 	#[serde(rename = "offset-y")]
-	offset_y: usize,
-	widgets: Vec<String>,
+	pub offset_y: i16,
+	pub widgets: Vec<String>,
 	#[serde(rename = "widget-spacing")]
-	widget_spacing: String,
+	pub widget_spacing: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-	bar: Bar,
-	widgets: HashMap<String, Widget>,
+	pub bar: Bar,
+	pub widgets: HashMap<String, Widget>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct TimeWidget {
-	format: Option<String>,
-	#[serde(rename = "foreground-normal")]
-	foreground_normal: Option<String>,
-	#[serde(rename = "background-normal")]
-	background_normal: Option<String>,
-	#[serde(rename = "foreground-hover")]
-	foreground_hover: Option<String>,
-	#[serde(rename = "background-hover")]
-	background_hover: Option<String>,
+	#[serde(default = "default_time")]
+	pub format: String,
+	#[serde(rename = "foreground-normal", default = "default_black")]
+	pub foreground_normal: String,
+	#[serde(rename = "background-normal", default = "default_white")]
+	pub background_normal: String,
+	#[serde(rename = "foreground-hover", default = "default_black")]
+	pub foreground_hover: String,
+	#[serde(rename = "background-hover", default = "default_white")]
+	pub background_hover: String,
 }
 
 #[serde(tag = "type")]
@@ -41,4 +43,20 @@ pub struct TimeWidget {
 pub enum Widget {
 	#[serde(rename = "time")]
 	Time(TimeWidget),
+}
+
+fn default_false() -> bool {
+	false
+}
+fn default_zero() -> u16 {
+	0
+}
+fn default_white() -> String {
+	"#ffffff".into()
+}
+fn default_black() -> String {
+	"#000000".into()
+}
+fn default_time() -> String {
+	"%H:%M".into()
 }
