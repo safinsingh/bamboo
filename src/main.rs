94346a36ac043e579fe454be8068b5f5f882417a
safinsingh <safin.singh@gmail.com>
2020-10-22 13:57:28 -0700

    command line parsing, abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
 extern crate x11rb;
 
-use std::{error::Error, fs};
+use clap::Clap;
+use std::{
+	error::Error,
+	fs,
+	sync::{
+		atomic::{AtomicBool, Ordering},
+		Arc,
+	},
+};
 use x11rb::connection::Connection;
 
 mod lib;
 use lib::*;
 
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "safinsingh <safin.singh@gmail.com>")]
+struct Opts {
+	/// Location of configuration file
+	#[clap(short, long, default_value = "bamboo.toml")]
+	config: String,
+
+	/// Name of bar to display
+	#[clap(short, long, default_value = "default")]
+	bar: String,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-	let read = fs::read_to_string("bamboo.toml")
-		.expect("Couldn't find bamboo.toml!");
-	let conf: Config =
-		toml::from_str(&read).expect("Couldn't deserialize bamboo.toml!");
+	let opts: Opts = Opts::parse();
+	let read = fs::read_to_string(opts.config)?;
+	let conf: Config = toml::from_str(&read)?;
 
 	let (conn, screen_num) = x11rb::connect(None)?;
-
 	let screen = &conn.setup().roots[screen_num];
 	let win = conn.generate_id()?;
 
-	conf.bar.draw(&conn, screen, win)?;
+	let running = Arc::new(AtomicBool::new(true));
+	let r = running.clone();
 
-	std::thread::sleep(std::time::Duration::from_secs(10));
+	ctrlc::set_handler(move || {
+		r.store(false, Ordering::SeqCst);
+	})
+	.expect("Error setting Ctrl-C handler");
+
+	while running.load(Ordering::SeqCst) {
+		let bar = conf
+			.bar
+			.get(&opts.bar)
+			.ok_or(format!("Could not find bar: {}", opts.bar))?;
+		bar.draw(&conn, screen, win)?;
+	}
 
 	Ok(())
 }
